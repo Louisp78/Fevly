30ad98042fb59727063b15d6ebf536c2485c25fd
Louisp78 <louis.place@epita.fr>
2021-08-09 00:14:36 +0200

    :sparkles: init

--- fevly/lib/screens/search/search_screen.dart
+++ fevly/lib/screens/search/search_screen.dart
@@ -7,7 +7,7 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Body(),
     );
   }
