e625f4e67f70a77d8f72c17bf28e7d23fed91792
vincent libeskind <vincent.libeskind@gmail.com>
2021-08-12 18:20:40 +0200

    add bottom and top bar to party_info screen

--- fevly/lib/screens/party_info/components/body.dart
+++ fevly/lib/screens/party_info/components/body.dart
@@ -1,3 +1,5 @@
+import 'package:fevly/components/custom_basic_app_bar.dart';
+import 'package:fevly/components/custom_bottom_bar.dart';
 import 'package:fevly/screens/party_info/components/party_description.dart';
 import 'package:fevly/styles/colors.dart';
 import 'package:fevly/styles/effects.dart';
@@ -21,80 +23,111 @@ class Body extends StatelessWidget {
     final TextTheme textTheme =
         GoogleFonts.quicksandTextTheme(Theme.of(context).textTheme);
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(height: size.height * 0.15),
-          Container(
-            height: size.height,
-            width: size.width,
-            decoration: const BoxDecoration(
-              // ignore: unnecessary_const
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(45),
-                topLeft: Radius.circular(45),
-              ),
-              color: Colors.white,
-            ),
-            child: Column(
-              children: [
-                SizedBox(height: size.height * 0.03),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Organisateurs", style: textTheme.headline5),
-                      Text(
-                          "${listParties1[0].listOfOrganizerLength} organisateurs",
-                          style: textTheme.headline5),
-                    ],
+    return SizedBox(
+      height: size.height,
+      child: Stack(children: [
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: size.height * 0.25),
+              Container(
+                height: size.height - size.height * 0.05,
+                width: size.width,
+                decoration: const BoxDecoration(
+                  // ignore: unnecessary_const
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(45),
+                    topLeft: Radius.circular(45),
                   ),
+                  color: Colors.white,
                 ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ...List.generate(
-                          listParties1[0].listOfOrganizerLength,
-                          (index) => AvatarInfo(
-                                text:
-                                    listParties1[0].listOfOrganizer[index].name,
-                                size: size,
-                                textTheme: textTheme,
-                              )),
-                    ],
-                  ),
-                ),
-                SizedBox(height: size.height * 0.02),
-                PartyDescription(party: listParties1[0]),
-                SizedBox(height: size.height * 0.02),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      AddressInformation(party: listParties1[0]),
-                      Container(
-                        height: 150,
-                        width: (size.width * 0.9 / 2) - (size.width * 0.05 / 2),
-                        decoration: BoxDecoration(
-                          color: kSurfaceLightColor,
-                          boxShadow: [kShadowBase],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+                child: Column(
+                  children: [
+                    SizedBox(height: size.height * 0.03),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: size.width * 0.05),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Organisateurs", style: textTheme.headline5),
+                          Text(
+                              "${listParties1[0].listOfOrganizerLength} organisateurs",
+                              style: textTheme.headline5),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ...List.generate(
+                              listParties1[0].listOfOrganizerLength,
+                              (index) => AvatarInfo(
+                                    text: listParties1[0]
+                                        .listOfOrganizer[index]
+                                        .name,
+                                    size: size,
+                                    textTheme: textTheme,
+                                  )),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    PartyDescription(party: listParties1[0]),
+                    SizedBox(height: size.height * 0.02),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: size.width * 0.05),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AddressInformation(party: listParties1[0]),
+                          Container(
+                            height: 150,
+                            width: (size.width * 0.9 / 2) -
+                                (size.width * 0.05 / 2),
+                            decoration: BoxDecoration(
+                              color: kSurfaceLightColor,
+                              boxShadow: [kShadowBase],
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    const DateInformations()
+                  ],
                 ),
-                SizedBox(height: size.height * 0.02),
-                const DateInformations()
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        const Positioned(bottom: 0, child: CustomBottomBar()),
+        CustomBasicAppBar(
+          iconData: Icons.arrow_back_ios_rounded,
+          press: () {},
+          title: listParties1[0].name,
+          subtitle: "${listParties1[0].guestsLength} participants",
+          prefixIcon: IconButton(
+            icon: const Icon(Icons.info_rounded),
+            color: Colors.black,
+            onPressed: () {},
+          ),
+          firstSuffixIcon: IconButton(
+            icon: const Icon(Icons.alarm),
+            color: kSecondaryColor,
+            onPressed: () {},
+          ),
+          secondSuffixIcon: IconButton(
+            icon: const Icon(Icons.group_rounded),
+            color: Colors.black,
+            onPressed: () {},
+          ),
+        ),
+      ]),
     );
   }
 }
