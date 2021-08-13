0374d6cb87674944f87d85165494f4018e03cc67
Louisp78 <louis.place@epita.fr>
2021-08-13 07:56:42 +0200

    :wrench: fix dropdown

--- fevly/lib/screens/shopping/shopping_screen.dart
+++ fevly/lib/screens/shopping/shopping_screen.dart
@@ -1,11 +1,10 @@
-import 'package:fevly/styles/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'components/body.dart';
 
 class ShoppingScreen extends StatelessWidget {
-  const ShoppingScreen({ Key? key }) : super(key: key);
+  const ShoppingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
