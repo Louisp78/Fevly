b381d60c90e5e12383d3c09343750fcaa7b8c70a
vincent <vincent.libeskind@gmail.com>
2021-08-18 17:09:50 +0200

    add assets/illustrations and tutorial screen

---
+++ fevly/lib/screens/tutorial/tutorial_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class TutorialScreen extends StatelessWidget {
+  const TutorialScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
