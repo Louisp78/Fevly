9481aa2035f7adb535fd62edf06a27dd8569aa2c
louis.place <louis.place@epita.fr>
2022-04-18 18:42:57 +0200

    adding basics model and tests

--- fevly/lib/model/user.dart
+++ fevly/lib/model/user.dart
@@ -1,25 +1,124 @@
-
 import 'dart:math';
 
-abstract class User {
+import 'package:fevly/model/party.dart';
 
-  final double y = 2;
-  final double x = 0.07;
+class User {
+  // Private
+  final double _y = 2;
+  final double _x = 0.07;
 
+  // Public
   String login;
   String name;
   String password;
+  // Friends is a bijective relation
+  Set<User> listOfFriends;
+  Set<User> listOfMadeRequests;
+  Set<User> listOfReceivedRequests;
+  Set<User> listOfBlocked;
+
+  /// To differenciate party where user is organizer or not see Party.organizer
+  Set<Party> listOfParties;
   int xp;
 
-  User({
-    required this.login,
-    required this.name,
-    required this.password,
-    this.xp = 0});
+  User(
+      {required this.login,
+      required this.name,
+      required this.password,
+      this.xp = 0,
+      this.listOfFriends = const {},
+      this.listOfParties = const {},
+      this.listOfMadeRequests = const {},
+      this.listOfReceivedRequests = const {},
+      this.listOfBlocked = const {}});
 
-  int getLevelFromXp()
-  {
-    return (pow(xp, 1/y) * x).round();
+  int getLevelFromXp() {
+    return (pow(xp, 1 / _y) * _x).round();
   }
 
+  // Block user
+  bool blockUser(final User user) {
+    if (listOfBlocked.contains(user)) return false;
+
+    listOfFriends.remove(user);
+    listOfMadeRequests.remove(user);
+    listOfBlocked.add(user);
+    user.listOfFriends.remove(this);
+    user.listOfMadeRequests.remove(this);
+    return true;
+  }
+
+  // Unblock user
+  bool unblockUser(final User user) {
+    return listOfBlocked.remove(user);
+  }
+
+  // Accept request from user
+  bool acceptRequestFrom(final User user) {
+    if (listOfReceivedRequests.remove(user)) {
+      listOfFriends.add(user);
+      user.listOfMadeRequests.remove(this);
+      user.listOfFriends.add(this);
+      return true;
+    }
+    return false;
+  }
+
+  // Reject request from user
+  bool rejectRequest(final User user) {
+    return listOfReceivedRequests.remove(user);
+    // Dont remove this from user.listOfMadeRequests
+  }
+
+  // Send request to user
+  bool sendRequestTo(final User user) {
+    if (listOfFriends.contains(user) || isBlockedWith(user)) {
+      return false;
+    } else if (user.listOfMadeRequests.remove(this)) {
+      acceptRequestFrom(user);
+      return true;
+    }
+    user.listOfReceivedRequests.add(this);
+    return listOfMadeRequests.add(user);
+  }
+
+  bool undoRequestTo(final User user) {
+    user.listOfReceivedRequests.remove(this);
+    return listOfMadeRequests.remove(user);
+  }
+
+  bool removeFriend(final User user) {
+    final returnVal = listOfFriends.remove(user);
+    user.listOfFriends.remove(this);
+    return returnVal;
+  }
+
+  bool isFriendWith(final User user) {
+    return listOfFriends.contains(user);
+  }
+
+  bool haveEmitRequestTo(final User user) {
+    return listOfMadeRequests.contains(user);
+  }
+
+  bool haveReceivedRequestFrom(final User user) {
+    return listOfReceivedRequests.contains(user);
+  }
+
+  bool isBlockedWith(User user) {
+    return listOfBlocked.contains(user);
+  }
+
+  bool isInvitedToParty(Party party) {
+    return listOfParties.contains(party);
+  }
+
+  bool isOrganizerOfParty(Party party) {
+    return party.organizer == this;
+  }
+
+  @override
+  String toString() {
+    return "User(login: $login, name: $name, password: $password, xp: $xp)";
+  }
 }
---
+++ fevly/test/party.dart
@@ -0,0 +1,23 @@
+import 'package:fevly/model/chat.dart';
+import 'package:fevly/model/location.dart';
+import 'package:fevly/model/party.dart';
+import 'package:fevly/model/user.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("Party Basics", () {
+    var party = Party(
+        name: "Party 1",
+        organizer: User(login: "login", name: "name", password: "password"),
+        startDate: DateTime.now(),
+        endDate: DateTime.now(),
+        location: Location(
+            city: "Bailly",
+            address: "1 impasse du poirier au large",
+            country: "France"),
+        chat: Chat());
+    test("get name", () {
+      expect(party.name, "Party 1");
+    });
+  });
+}
