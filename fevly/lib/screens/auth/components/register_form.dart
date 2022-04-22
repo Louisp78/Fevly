e264517b3dbce8ff731a32577887d10805fe8dff
louis.place <louis.place@epita.fr>
2022-04-22 21:42:52 +0200

    auth basics + loading + anim

--- fevly/lib/screens/auth/components/register_form.dart
+++ fevly/lib/screens/auth/components/register_form.dart
@@ -1,8 +1,10 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:fevly/components/custom_loading_button.dart';
 import 'package:fevly/components/custom_text_button.dart';
 import 'package:fevly/components/custom_text_field.dart';
 import 'package:fevly/constant.dart';
 import 'package:fevly/errors_msg.dart';
+import 'package:fevly/screens/auth/view_models/auth_view_model.dart';
 import 'package:fevly/service/application_state.dart';
 import 'package:fevly/validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -39,12 +41,17 @@ class _RegisterFormState extends State<RegisterForm> {
         GoogleFonts.quicksandTextTheme(Theme.of(context).textTheme);
     final Size size = MediaQuery.of(context).size;
     final ColorScheme themeColor = Theme.of(context).colorScheme;
+    final AuthViewModel authVM = Provider.of<AuthViewModel>(context);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(
-          'Nouveau compte',
-          style: textTheme.displayLarge,
+        SizedBox(
+          width: size.width * 0.7,
+          child: AutoSizeText(
+            'Nouveau compte',
+            maxLines: 1,
+            style: textTheme.displayLarge,
+          ),
         ),
         Text(
           widget.email,
@@ -84,7 +91,10 @@ class _RegisterFormState extends State<RegisterForm> {
                       hintText: 'Mot de passe',
                       obscureText: true,
                       validator: (value) {
-                        if (!Kpassword_pattern.hasMatch(value!)) {
+                        /*if (!Kpassword_pattern.hasMatch(value!)) {
+                          return Kpassword_error_msg;
+                        }*/
+                        if (value!.isEmpty) {
                           return Kpassword_error_msg;
                         }
                         return null;
@@ -118,29 +128,43 @@ class _RegisterFormState extends State<RegisterForm> {
                   Spacer(),
                   Align(
                     alignment: Alignment.bottomCenter,
-                    child: CustomTextButton(
-                      press: () async {
-                        if (_formKey.currentState!.validate()) {
-                          try {
-                            await appState
-                                .registerAccount(
-                                  emailAddress: widget.email,
-                                  login: _loginController.text,
-                                  password: _passwordController.text,
-                                )
-                                .then((value) =>
-                                    setState(() => password_error_msg = null));
-                          } on FirebaseAuthException catch (e) {
-                            setState(() {
-                              password_error_msg = e.message;
-                            });
+                    child: SizedBox(
+                      width: size.width * 0.7,
+                      child: CustomLoadingButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            authVM.isLoading = true;
+                            try {
+                              await appState
+                                  .registerAccount(
+                                    emailAddress: widget.email,
+                                    login: _loginController.text,
+                                    password: _passwordController.text,
+                                  )
+                                  .then((value) =>
+                                      setState(() => password_error_msg = null))
+                                  .then((value) => authVM.isLoading = false);
+                            } on FirebaseAuthException catch (e) {
+                              setState(() {
+                                password_error_msg = e.message;
+                              });
+                            }
                           }
-                        }
-                      },
-                      textWidget: AutoSizeText(
-                        'Accepter les conditions d\'utilisations',
-                        style: textTheme.headline5!
-                            .copyWith(color: themeColor.background),
+                        },
+                        maxWidth: size.width * 0.7,
+                        text_is_loading: 'Chargement',
+                        text_not_loading:
+                            'Accepter les conditions d\'utilisations',
+                        text_color_is_loading: themeColor.onBackground,
+                        text_color_not_loading: null,
+                        background_color_is_loading: themeColor.onSurface,
+                        background_color_not_loading: null,
+                        is_loading: authVM.isLoading,
+                        /*textWidget: AutoSizeText(
+                          'Accepter les conditions d\'utilisations',
+                          style: textTheme.headline5!
+                              .copyWith(color: themeColor.background),
+                        ),*/
                       ),
                     ),
                   ),
---
+++ fevly/lib/screens/auth/view_models/auth_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+//$ CLASS
+//$ ================================================
+class AuthViewModel extends ChangeNotifier {
+  AuthViewModel();
+
+  Color _backgroundColor = Colors.transparent;
+  double _width = 0.0;
+  double _height = 0.0;
+
+  bool _is_loading = false;
+  bool get isLoading => _is_loading;
+  set isLoading(bool value) {
+    _is_loading = value;
+    notifyListeners();
+  }
+
+  set backgroundColor(Color value) {
+    _backgroundColor = value;
+    notifyListeners();
+  }
+
+  void setwidthAndHeightAndColor(double height, double width, Color color) {
+    _width = width;
+    _height = height;
+    _backgroundColor = color;
+    notifyListeners();
+  }
+
+  Color get backgroundColor => _backgroundColor;
+  double get width => _width;
+  double get height => _height;
+}
